f8ce5d925e8867065fcfb48642132b2656755322
Petr Bouchal <pbouchal@gmail.com>
2022-05-08 16:21:13 +0200

    handle GHA workflow
    keeping files out of the targets - download and load is one step

--- _targets.R
+++ _targets.R
@@ -19,6 +19,26 @@ options(clustermq.scheduler = "multicore",
 
 for (file in list.files("R", full.names = TRUE)) source(file)
 
+if (Sys.getenv("GITHUB_ACTIONS") == TRUE) c_mv_timeout_hrs <- 0
+
+load_files_local_l <- list(
+  tar_file(excel_files,
+           curl::curl_download(excel_urls, file.path(c_excel_dir, excel_paths)),
+           pattern = map(excel_urls, excel_paths)),
+  tar_target(dt_all, load_one_excel(excel_files), pattern = map(excel_files))
+)
+
+load_files_gha_l <- list(
+  tar_target(dt_all, download_and_load_one_excel(excel_files),
+             pattern = map(excel_urls, excel_paths))
+)
+
+if (Sys.getenv("GITHUB_ACTIONS") == TRUE) {
+  load_files_l <- load_files_gha_l
+}  else {
+  load_files_l <- load_files_local_l
+}
+
 list(
   tar_target(mv_urls, c_mv_urls),
   tar_target(name = excel_metadata,
